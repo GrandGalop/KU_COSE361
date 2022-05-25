ad59749921d2024c45f6ee755993d7699cdaa37c
GrandGalop <105308604+GrandGalop@users.noreply.github.com>
2022-05-25 21:20:12 +0900

    Add files via upload

---
+++ minicontest1/myAgents.py
@@ -0,0 +1,241 @@
+# myAgents.py
+# ---------------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+#
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+from game import Agent
+from searchProblems import PositionSearchProblem
+
+import util
+import time
+import search
+
+"""
+IMPORTANT
+`agent` defines which agent you will use. By default, it is set to ClosestDotAgent,
+but when you're ready to test your own agent, replace it with MyAgent
+"""
+def createAgents(num_pacmen, agent='ClosestDotAgent'):
+    return [eval(agent)(index=i) for i in range(num_pacmen)]
+
+class MyAgent(Agent):
+    """
+    Implementation of your agent.
+    """
+    def findPathToClosestDot(self, gameState):
+        """
+        Returns a path (a list of actions) to the closest dot, starting from
+        gameState.
+        """
+        # Here are some useful elements of the startState
+        startPosition = gameState.getPacmanPosition(self.index)
+        food = gameState.getFood()
+        walls = gameState.getWalls()
+        problem = AnyFoodSearchProblem(gameState, self.index)
+        "*** YOUR CODE HERE ***"
+
+        pacmanCurrent = [problem.getStartState(), [], 0]
+        visitedPosition = set()
+        # visitedPosition.add(problem.getStartState())
+        fringe = util.PriorityQueue()
+        fringe.push(pacmanCurrent, pacmanCurrent[2])
+        while not fringe.isEmpty():
+            pacmanCurrent = fringe.pop()
+            if pacmanCurrent[0] in visitedPosition:
+                continue
+            else:
+                visitedPosition.add(pacmanCurrent[0])
+            if problem.isGoalState(pacmanCurrent[0]):
+                return pacmanCurrent[0], pacmanCurrent[1] # 이 시점에서 pacmanCurrent[0]은 최근접 food의 위치를 저장하므로 경로와 함께 return을 시킴.
+            else:
+                pacmanSuccessors = problem.getSuccessors(pacmanCurrent[0])
+            Successor = []
+            for item in pacmanSuccessors:  # item: [(x,y), 'direction', cost]
+                if item[0] not in visitedPosition:
+                    pacmanRoute = pacmanCurrent[1].copy()
+                    pacmanRoute.append(item[1])
+                    sumCost = pacmanCurrent[2]
+                    Successor.append([item[0], pacmanRoute, sumCost + item[2]])
+            for item in Successor:
+                fringe.push(item, item[2])
+        return pacmanCurrent[0], pacmanCurrent[1]
+    
+    def findPathofAgent(self, gameState, num): # index가 num인 agent가 찾아 가는 food의 위치를 파악하기 위한 부분.
+        """
+        Returns a path (a list of actions) to the closest dot, starting from
+        gameState.
+        """
+        # Here are some useful elements of the startState
+        startPosition = gameState.getPacmanPosition(num)
+        food = gameState.getFood()
+        walls = gameState.getWalls()
+        problem = AnyFoodSearchProblem(gameState, num)
+
+        "*** YOUR CODE HERE ***"
+        pacmanCurrent = [problem.getStartState(), [], 0]
+        visitedPosition = set()
+        # visitedPosition.add(problem.getStartState())
+        fringe = util.PriorityQueue()
+        fringe.push(pacmanCurrent, pacmanCurrent[2])
+        while not fringe.isEmpty():
+            pacmanCurrent = fringe.pop()
+            if pacmanCurrent[0] in visitedPosition:
+                continue
+            else:
+                visitedPosition.add(pacmanCurrent[0])
+            if problem.isGoalState(pacmanCurrent[0]):
+                return pacmanCurrent[0], pacmanCurrent[1] # GoalState를 만족 시 pacmanCurrent[0]이 최근접 이게 됨.
+            else:
+                pacmanSuccessors = problem.getSuccessors(pacmanCurrent[0])
+            Successor = []
+            for item in pacmanSuccessors:  # item: [(x,y), 'direction', cost]
+                if item[0] not in visitedPosition:
+                    pacmanRoute = pacmanCurrent[1].copy()
+                    pacmanRoute.append(item[1])
+                    sumCost = pacmanCurrent[2]
+                    Successor.append([item[0], pacmanRoute, sumCost + item[2]])
+            for item in Successor:
+                fringe.push(item, item[2])
+        return pacmanCurrent[0], pacmanCurrent[1]
+
+
+    """
+    makevirtualstate 함수 : index=num이 찾아가는 food가 없는 가상의 virtualstate를 return하는 함수.
+    """
+    def makevirtualstate(self, state, num):
+        foodpos, pathway = self.findPathofAgent(state, num)
+        x, y=foodpos[0], foodpos[1] # findPathofAgent함수의 튜플 첫 값이 최근접 food의 위치임
+        state.data.food[x][y]=False #해당 food 위치에 food가 없는 것으로 간주할 것임.
+        return state
+
+
+    def getAction(self, state):
+
+        """
+        Returns the next action the agent will take
+        """
+        from game import Directions
+        if self.index==0:
+            foodpos, pathway = self.findPathToClosestDot(state)            
+            return pathway[0]
+        else:
+            if state.getNumFood()>10:
+            # map상의 Food가 11개 이상일 경우엔, 이전의 index agent가 targeting한 food를 제외하고 closest food를 찾는다.
+            # 11이라는 숫자는 임의로 설정한 숫자로, 원래는 다양한 시도를 통해 optimal한 score를 return하는 것이 많으나, 시간 관계상 임의로 설정함.
+                virtualstate = self.makevirtualstate(state, self.index-1) # virtualstate는 직전 agent가 targeting하는 food를 없는 것으로 간주.
+                foodpos, pathway = self.findPathToClosestDot(virtualstate) # virtualstate의 ClosestDot을 찾는 pathway를 return하여
+                return pathway[0] #그 경로의 첫 action을 return한다.
+            else:
+            # map상에 Food가 10개 이하의 경우, ClosestDotAgent처럼 활동하게 바뀐다.
+                foodpos, pathway = self.findPathToClosestDot(state)     
+                return pathway[0]
+        # raise NotImplementedError()        
+
+        "*** YOUR CODE HERE ***"
+
+
+    def initialize(self):
+        """
+        Intialize anything you want to here. This function is called
+        when the agent is first created. If you don't need to use it, then
+        leave it blank
+        """
+
+        "*** YOUR CODE HERE"
+        # raise NotImplementedError()        
+
+
+"""
+Put any other SearchProblems or search methods below. You may also import classes/methods in
+search.py and searchProblems.py. (ClosestDotAgent as an example below)
+"""
+
+class ClosestDotAgent(Agent):
+
+    def findPathToClosestDot(self, gameState):
+        """
+        Returns a path (a list of actions) to the closest dot, starting from
+        gameState.
+        """
+        # Here are some useful elements of the startState
+        startPosition = gameState.getPacmanPosition(self.index)
+        food = gameState.getFood()
+        walls = gameState.getWalls()
+        problem = AnyFoodSearchProblem(gameState, self.index)
+        
+
+        "*** YOUR CODE HERE ***"
+
+        pacmanCurrent = [problem.getStartState(), [], 0]
+        visitedPosition = set()
+        # visitedPosition.add(problem.getStartState())
+        fringe = util.PriorityQueue()
+        fringe.push(pacmanCurrent, pacmanCurrent[2])
+        while not fringe.isEmpty():
+            pacmanCurrent = fringe.pop()
+            if pacmanCurrent[0] in visitedPosition:
+                continue
+            else:
+                visitedPosition.add(pacmanCurrent[0])
+            if problem.isGoalState(pacmanCurrent[0]):
+                return pacmanCurrent[1]
+            else:
+                pacmanSuccessors = problem.getSuccessors(pacmanCurrent[0])
+            Successor = []
+            for item in pacmanSuccessors:  # item: [(x,y), 'direction', cost]
+                if item[0] not in visitedPosition:
+                    pacmanRoute = pacmanCurrent[1].copy()
+                    pacmanRoute.append(item[1])
+                    sumCost = pacmanCurrent[2]
+                    Successor.append([item[0], pacmanRoute, sumCost + item[2]])
+            for item in Successor:
+                fringe.push(item, item[2])
+        return pacmanCurrent[1]
+
+    def getAction(self, state):
+        return self.findPathToClosestDot(state)[0]
+
+class AnyFoodSearchProblem(PositionSearchProblem):
+    """
+    A search problem for finding a path to any food.
+
+    This search problem is just like the PositionSearchProblem, but has a
+    different goal test, which you need to fill in below.  The state space and
+    successor function do not need to be changed.
+
+    The class definition above, AnyFoodSearchProblem(PositionSearchProblem),
+    inherits the methods of the PositionSearchProblem.
+
+    You can use this search problem to help you fill in the findPathToClosestDot
+    method.
+    """
+
+    def __init__(self, gameState, agentIndex):
+        "Stores information from the gameState.  You don't need to change this."
+        # Store the food for later reference
+        self.food = gameState.getFood() # GameState class의 getFood() 함수는 Boolean matrix를 return
+
+        # Store info for the PositionSearchProblem (no need to change this)
+        self.walls = gameState.getWalls()
+        self.startState = gameState.getPacmanPosition(agentIndex)
+        self.costFn = lambda x: 1
+        self._visited, self._visitedlist, self._expanded = {}, [], 0 # DO NOT CHANGE
+
+    def isGoalState(self, state):
+        """
+        The state is Pacman's position. Fill this in with a goal test that will
+        complete the problem definition.
+        """
+        x,y = state
+        if self.food[x][y] == True:
+            return True
+        return False
+
